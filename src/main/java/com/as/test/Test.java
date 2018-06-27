e8fce48e585bc6348aa7c0ca6482da3687deec1c
as <88452486@qq.com>
2018-06-27 19:56:50 +0800

    SCS-V1.0.0：新的开始

---
+++ src/main/java/com/as/test/Test.java
@@ -0,0 +1,11 @@
+package com.as.test;
+
+/**
+ * Description:
+ * Created by as on 2018/6/27.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
